61b182ded78000c1be8c6ccca97e684e899d5d67
Servidor <dce.guytorres@gmil.com>
2025-02-07 02:29:52 +0000

    att bd4

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["82.29.57.23", "dceguytorres.com", "127.0.0.1"]
 
